5339c4160cdbbb010d39945840dd81f074335b2b
Sergey <phonar@list.ru>
2022-08-22 16:29:02 +0300

    Calculating the max of two numbers

---
+++ Sem1/Sem_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+int max = number1;
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 < number2) Console.WriteLine(number2);
+else if (number1 == number2) Console.WriteLine("Числа равны."); 
+else Console.WriteLine(number1);
