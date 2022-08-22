7a62b7fee93137312ad7ad986487f0c292de841f
Sergey <phonar@list.ru>
2022-08-22 18:26:15 +0300

    Calculate pos. or neg numbers

---
+++ Sem1/Sem_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 4;
+if (a % 2 == 1) Console.WriteLine($"{a} нечётное число");
+else if (a % 2 == -1) Console.WriteLine($"{a} нечётное число");
+else Console.WriteLine($"{a} число чётное");
+
+a = -3;
+if (a % 2 == 1) Console.WriteLine($"{a} нечётное число");
+else if (a % 2 == -1) Console.WriteLine($"{a} нечётное число");
+else Console.WriteLine($"{a} число чётное");
+
+a = 7;
+if (a % 2 == 1) Console.WriteLine($"{a} нечётное число");
+else if (a % 2 == -1) Console.WriteLine($"{a} нечётное число");
+else Console.WriteLine($"{a} число чётное");
