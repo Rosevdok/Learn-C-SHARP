6724045573fab70dd13352fe1629d8164a3d5e47
Sergey <phonar@list.ru>
2022-08-22 17:06:11 +0300

    Calculating the max of three numbers

---
+++ Sem1/Sem_2/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine(max);
+
+a = 44;
+b = 5;
+c = 78;
+max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine(max);
+
+a = 22;
+b = 3;
+c = 9;
+max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine(max);
