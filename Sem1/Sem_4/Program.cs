4094427afdf36c615bc99ebff89663615955ff09
Sergey <phonar@list.ru>
2022-08-23 20:28:08 +0300

    Calculate an even number

---
+++ Sem1/Sem_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine());
+int even = 2;
+if (even > N) Console.WriteLine(even);
+else 
+{
+    while (even < N)
+    {
+    Console.WriteLine(even);
+    even = even + 2;
+    }
+}
