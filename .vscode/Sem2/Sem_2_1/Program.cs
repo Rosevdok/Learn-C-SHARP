dab55f47df799095abc9eebcd12fee904e555d90
Sergey <phonar@list.ru>
2022-08-23 21:32:34 +0300

    Definition of tens

---
+++ .vscode/Sem2/Sem_2_1/Program.cs
@@ -0,0 +1,23 @@
+// На входе число 456
+int num = 456;
+
+int doz = num / 10;
+int ones = doz % 10;
+
+Console.WriteLine(ones);
+
+// На входе число 782
+num = 782;
+
+doz = num / 10;
+ones = doz % 10;
+
+Console.WriteLine(ones);
+
+// На входе число 918
+num = 918;
+
+doz = num / 10;
+ones = doz % 10;
+
+Console.WriteLine(ones);
