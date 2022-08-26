f05583554e20be6cf5dd038b91fbce1aa508513e
Sergey <phonar@list.ru>
2022-08-26 16:42:34 +0300

    Modifickate prog

--- Sem1/Sem_4/Program.cs
+++ Sem1/Sem_4/Program.cs
@@ -1,12 +1,8 @@
 ﻿Console.Write("Введите целое число: ");
 int N = int.Parse(Console.ReadLine());
 int even = 2;
-if (even > N) Console.WriteLine(even);
-else 
+while (even <= N)
 {
-    while (even < N)
-    {
     Console.WriteLine(even);
     even = even + 2;
-    }
 }
