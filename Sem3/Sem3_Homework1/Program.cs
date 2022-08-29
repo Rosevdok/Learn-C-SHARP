996c0315fef8308200224636e777dce581d074fa
Sergey <phonar@list.ru>
2022-08-29 18:35:19 +0300

    Homework 3

---
+++ Sem3/Sem3_Homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+int remains = 0;
+
+void NamDig()
+{
+    remains = number % 10;
+    number = number / 10;
+}
+
+NamDig();
+int a1 = remains;
+NamDig();
+int a2 = remains;
+NamDig();
+int a3 = remains;
+NamDig();
+int a4 = remains;
+NamDig();
+int a5 = remains;
+
+if(a1 == a5 & a2 == a4)
+{
+    Console.WriteLine("Число является паллиндромом");
+}
+else 
+{
+    Console.WriteLine("Число не является паллиндромом");
+}
---
+++ Sem3/Sem3_Homework3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Write($"Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+if (num == 0) Console.WriteLine("0 в кубе равен нулю");
+for (int count = 1; count <= num; count++)
+{
+    double cube = Math.Pow(count, 3);
+    Console.WriteLine($"{count} в кубе равно {cube}: ");
+}
